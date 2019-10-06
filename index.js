efb2e767c0abbabd25e968a78bbd6d2d641659f4
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-06 21:44:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution in this file!
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, name, address) {
+  const newObj = {driver, {[name]:'Sam'}, {[address]: '11 Broadway'}};
+}
